64566926fbf4903bbe83932fd681b1c17bfb0189
MatheusCandido <matheus.csilva@bandtec.com.br>
2020-09-17 17:37:22 -0300

    Updates

---
+++ precache-manifest.c91ed934d2254cda4890cae2d087c44d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8038d9b5c7bfdcae2ed01b70a85422e7",
+    "url": "/github-pages-dsPesquisa/index.html"
+  },
+  {
+    "revision": "d271ad08591cbfed6a9e",
+    "url": "/github-pages-dsPesquisa/static/css/main.3c3308dc.chunk.css"
+  },
+  {
+    "revision": "558e792fbd05c5e64c2a",
+    "url": "/github-pages-dsPesquisa/static/js/2.5ae23a94.chunk.js"
+  },
+  {
+    "revision": "54815be02e01897f642aa6f239b15dec",
+    "url": "/github-pages-dsPesquisa/static/js/2.5ae23a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d271ad08591cbfed6a9e",
+    "url": "/github-pages-dsPesquisa/static/js/main.e44eba20.chunk.js"
+  },
+  {
+    "revision": "90974e725be41dffae3e",
+    "url": "/github-pages-dsPesquisa/static/js/runtime-main.486c54c6.js"
+  },
+  {
+    "revision": "8d5d159ed7f4dddb22c59da9958c01e3",
+    "url": "/github-pages-dsPesquisa/static/media/GAMER.8d5d159e.svg"
+  },
+  {
+    "revision": "c08814321789f1d603b3a5cc2de108a5",
+    "url": "/github-pages-dsPesquisa/static/media/Seta.c0881432.svg"
+  },
+  {
+    "revision": "efb918734459ad437c0e3127c2f97235",
+    "url": "/github-pages-dsPesquisa/static/media/logo.efb91873.svg"
+  }
+]);
